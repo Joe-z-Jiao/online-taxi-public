ffde6cdd16a3f697e161a5cf2823e1b2bedcca02
begc <47102817+begc@users.noreply.github.com>
2023-01-10 16:15:02 +0800

    service-passenger-user注册到nacos。

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -20,12 +20,10 @@ public class UserService {
     @Autowired
     private PassengerUserMapper passengerUserMapper;
     public ResponseResult loginOrRegister(String passengerPhone) {
-        System.out.println("user service被调用手机号为：" + passengerPhone);
         //根据手机号查询用户信息
         Map<String, Object> map = new HashMap<>();
         map.put("passenger_phone",passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.size() == 0?"无记录":passengerUsers.get(0).getPassengerPhone());
         //判断用户是否存在
         if (passengerUsers.size() == 0) {
             //若不存在，创建用户
