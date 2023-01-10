b5e289a2d4b45a914debd15e601aa1a576dbb015
begc <47102817+begc@users.noreply.github.com>
2023-01-10 14:18:40 +0800

    校验验证码功能编写完成。

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,10 +1,12 @@
 package com.mashibing.apipassenger.service;
 
 import com.mashibing.apipassenger.remote.ServiceVerificationCodeClient;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
 import com.mashibing.internalcommon.response.TokenResponse;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -31,10 +33,19 @@ public class VerificationCodeService  {
         ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationCodeClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
-        String key = verificationCodePrefix + phoneNum;
+        String key = generatorKeyByPhone(phoneNum);
         //存入redis中
         stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
         return ResponseResult.success("");
+    }
+
+    /**
+     * 根据手机号生成key
+     * @param phoneNum
+     * @return
+     */
+    private String generatorKeyByPhone(String phoneNum){
+        return verificationCodePrefix + phoneNum;
     }
 
     /**
@@ -44,7 +55,23 @@ public class VerificationCodeService  {
      * @return
      */
     public ResponseResult checkCode(String passengerPhone,String verificationCode){
-        System.out.println("去redis里面获取手机号和验证码");
+        //去redis里面获取手机号和验证码
+        //生成key
+        String key = generatorKeyByPhone(passengerPhone);
+        String redisCode = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("通过redis获取到的key" + redisCode);
+
+        //校验验证码
+        if (StringUtils.isBlank(redisCode)) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        //传入的验证码与用户输入的验证码不匹配
+        if (!verificationCode.trim().equals(redisCode.trim())) {
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+
+        }
+
+
         System.out.println("校验手机号和验证码是否匹配");
         System.out.println("判断原来是否有这个用户，并做后续处理");
         System.out.println("颁发令牌");
--- internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum CommonStatusEnum {
+    VERIFICATION_CODE_ERROR(1099,"验证码不正确"),
     SUCCESS(1,"SUCCESS"),
     FAIL(0,"FAIL ")
     ;
