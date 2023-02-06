eb0b7d2f07ba133bd5141e4db3cfa2a6b696df15
begc <47102817+begc@users.noreply.github.com>
2023-02-06 15:57:42 +0800

    api-boss 调用 serviceDriverUser服务绑定与解绑。

---
+++ api-boss/src/main/java/com/mashibing/apiboss/controller/DriverCarBindingRelationshipController.java
@@ -0,0 +1,27 @@
+package com.mashibing.apiboss.controller;
+
+import com.mashibing.apiboss.service.DriverCarBindingRelationshipService;
+import com.mashibing.internalcommon.dto.DriverCarBindingRelationship;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/driver-car-binding-relationship")
+public class DriverCarBindingRelationshipController {
+    @Autowired
+    public DriverCarBindingRelationshipService driverCarBindingRelationshipService;
+
+    @PostMapping("/bind")
+    public ResponseResult bind(@RequestBody DriverCarBindingRelationship driverCarBindingRelationship){
+        return driverCarBindingRelationshipService.bind(driverCarBindingRelationship);
+    }
+
+    @PostMapping("/unbind")
+    public ResponseResult unbind(@RequestBody DriverCarBindingRelationship driverCarBindingRelationship){
+        return driverCarBindingRelationshipService.unbind(driverCarBindingRelationship);
+    }
+}
---
+++ api-boss/src/main/java/com/mashibing/apiboss/service/DriverCarBindingRelationshipService.java
@@ -0,0 +1,23 @@
+package com.mashibing.apiboss.service;
+
+import com.mashibing.apiboss.controller.DriverCarBindingRelationshipController;
+import com.mashibing.apiboss.remote.ServiceDriverUserClient;
+import com.mashibing.internalcommon.dto.DriverCarBindingRelationship;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DriverCarBindingRelationshipService {
+
+    @Autowired
+    public ServiceDriverUserClient serviceDriverUserClient;
+
+    public ResponseResult bind(DriverCarBindingRelationship driverCarBindingRelationship){
+        return serviceDriverUserClient.bind(driverCarBindingRelationship);
+    }
+
+    public ResponseResult unbind(DriverCarBindingRelationship driverCarBindingRelationship){
+        return serviceDriverUserClient.unbind(driverCarBindingRelationship);
+    }
+}
