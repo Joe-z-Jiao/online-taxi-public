e501f78df86cb43830027170dfb158743fec28b7
begc <47102817+begc@users.noreply.github.com>
2023-02-08 10:03:09 +0800

    api-driver 调用 service-verificationcode 校验手机验证码服务完成。

--- api-driver/src/main/java/com/mashibing/apidriver/service/VerificationCodeService.java
+++ api-driver/src/main/java/com/mashibing/apidriver/service/VerificationCodeService.java
@@ -1,10 +1,12 @@
 package com.mashibing.apidriver.service;
 
 import com.mashibing.apidriver.remote.ServiceDriverUserClients;
+import com.mashibing.apidriver.remote.ServiceVerificationCodeClient;
 import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.constant.DriverCarConstants;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.DriverUserResponse;
+import com.mashibing.internalcommon.response.NumberCodeResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ public class VerificationCodeService {
     @Autowired
     public ServiceDriverUserClients serviceDriverUserClients;
 
+    @Autowired
+    public ServiceVerificationCodeClient codeClient;
+
     public ResponseResult checkAndsendVerificationCode(String driverPhone){
         // 查询service-user是否存在
         ResponseResult<DriverUserResponse> driverUserResponseResponseResult = serviceDriverUserClients.checkDriver(driverPhone);
@@ -27,7 +32,10 @@ public class VerificationCodeService {
         }
         log.info(driverPhone + "的司机村存在");
         // 获取验证码
-
+        ResponseResult<NumberCodeResponse> codeResult = codeClient.numberCode(6);
+        NumberCodeResponse resultData = codeResult.getData();
+        int numberCode = resultData.getNumberCode();
+        log.info("验证码" + numberCode);
         //调用第三方发送验证码
 
         //存入redis
