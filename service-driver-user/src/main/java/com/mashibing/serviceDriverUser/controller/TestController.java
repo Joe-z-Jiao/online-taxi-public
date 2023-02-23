5b7d8e35b4406034d02c9945e62283920afe5449
begc <47102817+begc@users.noreply.github.com>
2023-02-23 21:37:58 +0800

    测试 mybatis mapper 文件使用。

--- service-driver-user/src/main/java/com/mashibing/serviceDriverUser/controller/TestController.java
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.mashibing.serviceDriverUser.controller;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.serviceDriverUser.mapper.DriverUserMapper;
 import com.mashibing.serviceDriverUser.service.DriverUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +21,13 @@ public class TestController {
     @GetMapping("test-db")
     public ResponseResult testDb(){
         return driverUserService.getDriverUser();
+    }
+
+    @Autowired
+    public DriverUserMapper driverUserMapper;
+    @GetMapping("/test-xml")
+    public int testXml(String cityCode){
+        int i = driverUserMapper.select1(cityCode);
+        return i;
     }
 }
--- service-driver-user/src/main/java/com/mashibing/serviceDriverUser/mapper/DriverUserMapper.java
+++ service-driver-user/src/main/java/com/mashibing/serviceDriverUser/mapper/DriverUserMapper.java
@@ -2,9 +2,10 @@ package com.mashibing.serviceDriverUser.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mashibing.internalcommon.dto.DriverUser;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverUserMapper extends BaseMapper<DriverUser> {
-
+    public int select1(@Param("cityCode") String cityCode);
 }
