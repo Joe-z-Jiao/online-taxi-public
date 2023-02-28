f3aa5b5f7928cc39b085c69fd002b3bf53ce72f9
begc <47102817+begc@users.noreply.github.com>
2023-02-28 16:28:22 +0800

    司机发起收款代码完成。

---
+++ api-driver/src/main/java/com/mashibing/apidriver/controller/PayController.java
@@ -0,0 +1,28 @@
+package com.mashibing.apidriver.controller;
+
+import com.mashibing.apidriver.service.PayService;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pay")
+public class PayController {
+
+    @Autowired
+    private PayService payService;
+
+    /**
+     * 司机发起收款
+     * @param orderId
+     * @param price
+     * @return
+     */
+    @PostMapping("/push-pay-info")
+    public ResponseResult pushPayInfo(@RequestParam String orderId,@RequestParam String price,@RequestParam Long passengerId){
+        return payService.pushPayInfo(orderId,price,passengerId);
+    }
+}
---
+++ api-driver/src/main/java/com/mashibing/apidriver/service/PayService.java
@@ -0,0 +1,26 @@
+package com.mashibing.apidriver.service;
+
+import com.mashibing.apidriver.remote.ServiceSsePushClient;
+import com.mashibing.internalcommon.constant.IdentityConstant;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PayService {
+
+    @Autowired
+    ServiceSsePushClient serviceSsePushClient;
+
+    public ResponseResult pushPayInfo(String orderId, String price, Long passengerId) {
+        //接收消息
+        JSONObject message = new JSONObject();
+        message.put("price", price);
+        message.put("orderId",orderId);
+
+        //推送消息
+        serviceSsePushClient.push(passengerId, IdentityConstant.PASSENGER_IDENTITY,message.toString());
+        return ResponseResult.success("");
+    }
+}
